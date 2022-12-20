b4613ddebcbdf417e812399d16f46d757983c32c
saviooribe <saviooribe@gmail.com>
2022-12-20 12:05:54 -0300

    Estado (useState)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,12 @@
 import { format, formatDistanceToNow } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+import { useState } from "react";
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post({ publishedAt, author, content }) {
+  const [comments, setComments] = useState([1, 2]);
   const publishedAtDateFormatted = format(
     publishedAt,
     "d 'de' LLLL 'ás' HH:mm'h'",
@@ -17,6 +19,11 @@ export function Post({ publishedAt, author, content }) {
     locale: ptBR,
     addSuffix: true,
   });
+
+  function handleCreateNewComment(e) {
+    e.preventDefault();
+    setComments((old) => [...old, old.length]);
+  }
 
   return (
     <article className={styles.post}>
@@ -48,7 +55,7 @@ export function Post({ publishedAt, author, content }) {
           }
         })}
       </div>
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
         <textarea placeholder="Deixe um comentário" />
         <footer>
@@ -56,9 +63,9 @@ export function Post({ publishedAt, author, content }) {
         </footer>
       </form>
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map((comment) => {
+          return <Comment />;
+        })}
       </div>
     </article>
   );

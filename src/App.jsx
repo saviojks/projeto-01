51facb1b7f8bfd1a16695d8c4833234eac085bec
saviooribe <saviooribe@gmail.com>
2022-12-20 10:40:52 -0300

    Iterando no JSX

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,63 @@ import styles from "./App.module.css";
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 
+const posts = [
+  {
+    id: "0ed5d3d0-ea69-4c4d-a21d-6a38a8d68925",
+    email: "saviojks@gmail.com",
+    level: 1,
+    author: {
+      name: "Sávio jks",
+      avatarUrl: "http://github.com/saviojks.png",
+      role: "Full Steak",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          " Acabei de subir mais um projeto no meu portifa. É um projeto que fiz",
+      },
+      {
+        type: "paragraph",
+        content:
+          "no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+      { type: "link", content: "#novoprojeto" },
+      { type: "link", content: "#nlw 👋" },
+    ],
+    publishedAt: new Date("2022-12-16T18:08:10.786Z"),
+  },
+  {
+    id: "01d8ca39-9fa6-4d44-a7e6-226ca1a8bd06",
+    email: "saviojks@gmail.com",
+    level: 1,
+    author: {
+      name: "Diego #",
+      avatarUrl: "http://github.com/diego3.png",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          " Acabei de subir mais um projeto no meu portifa. É um projeto que fiz",
+      },
+      {
+        type: "paragraph",
+        content:
+          "no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+      { type: "link", content: "#novoprojeto" },
+      { type: "link", content: "#nlw 👋" },
+    ],
+    publishedAt: new Date("2022-12-16T18:18:37.053Z"),
+  },
+];
+
 function App() {
   return (
     <div>
@@ -10,18 +67,9 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Sávio jks"
-            content={
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tenetur beatae sit nemo doloribus dolorum architecto consequuntur commodi sequi, sed atque id fugit, aliquam exercitationem delectus, ducimus molestiae earum iure!"
-            }
-          />
-          <Post
-            author="Alessandra"
-            content={
-              "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis quae laboriosam repellendus deleniti aperiam consequatur minus, officiis ipsam ducimus. Nemo esse id suscipit, itaque dolorem natus quis iure repellat enim."
-            }
-          />
+          {posts.map((post) => {
+            return <Post author={post.author} content={post.content} />;
+          })}
         </main>
       </div>
     </div>
